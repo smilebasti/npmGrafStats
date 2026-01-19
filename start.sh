66f8a3f316411c2448626b8de08777ee2decffb6
smilebasti <60941345+smilebasti@users.noreply.github.com>
2026-01-19 14:19:29 +0100

    plus-v3.1.1

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "npmGrafStats:plus-v3.1.0-pre"
+echo "npmGrafStats: v3.1.1-pre"
 echo "Startup: lets get the logs send them to influx"
 
 if [ -z "$INFLUX_TOKEN" ] && [ ! -f "/data/influxdb-token.txt" ]; then
@@ -8,11 +8,25 @@ if [ -z "$INFLUX_TOKEN" ] && [ ! -f "/data/influxdb-token.txt" ]; then
     exit 1
 fi
 
-bash /root/.config/NPMGRAF/sendips.sh
+if [ "$REDIRECTION_LOGS" = "TRUE" ]
+then
+    echo "Redirection and Reverse-Proxy Logs activated"
+    bash /home/appuser/.config/NPMGRAF/sendips.sh &
+    bash /home/appuser/.config/NPMGRAF/sendredirectionips.sh &
+
+elif [ "$REDIRECTION_LOGS" = "ONLY" ]
+then
+    echo "Only Redirection Logs activated"
+    bash /home/appuser/.config/NPMGRAF/sendredirectionips.sh
+
+else
+    echo "Only Reverse-Proxy Logs activated"
+    bash /home/appuser/.config/NPMGRAF/sendips.sh
+fi
 
 sleep 0.5
 # reads from standard input and writes to standard output and in file
-tee /root/.config/NPMGRAF/nohup.out > /proc/1/fd/1 2>/proc/1/fd/2
+tee /home/appuser/.config/NPMGRAF/nohup.out > /proc/1/fd/1 2>/proc/1/fd/2
 
 # Wait for any process to exit
 wait -n
